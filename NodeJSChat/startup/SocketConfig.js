7d1f917d00a6cce5ee86d692e0aa394f7da1b6d8
Yury Kabernik-Berazouski <kobernykbeljr@gmail.com>
2018-10-27 20:15:34 +0300

    Add server sockets events processing

--- NodeJSChat/startup/SocketConfig.js
+++ NodeJSChat/startup/SocketConfig.js
@@ -1,4 +1,5 @@
 import io from 'socket.io';
+import Store from 'data-store';
 
 export default class SocketConfig {
 
@@ -7,6 +8,43 @@ export default class SocketConfig {
     }
 
     run() {
+        let localIO = this._io;
+        let date = new Date();
+        let storeMessages = new Store({ path: './data_storage/messages.json' });
+        let storeUsers = new Store({ path: './data_storage/users.json' });
 
+        // для обработки нового события через socket, добавь нового обработчика socket.on() 
+        localIO.sockets.on('connection', function (socket) {
+
+            socket.on('register', function (userName) {
+
+                let clientID = socket.client.id;
+                console.log('New user: ' + userName);
+                socket.userName = userName;
+                storeUsers.load();
+                storeUsers.set(clientID, { 'name': userName });
+
+                localIO.emit('new user', userName);
+            });
+
+            socket.on('chat message', function (msg) {
+
+                let message = '<div class="msg-container darker"><img src="img/img_avatar2.png" alt="Avatar" class="right"><strong>' + msg.user + '</strong><p>' + msg.text + '</p> <span class="time-left">' + date.getHours() + ':' + date.getUTCMinutes() + '</span></div>';
+                let broadcastMessage = '<div class="msg-container"><img src="img/img_avatar2.png" alt="Avatar"><strong>' + msg.user + '</strong><p>' + msg.text + '</p> <span class="time-right">' + date.getHours() + ':' + date.getUTCMinutes() + '</span></div>';
+
+                storeMessages.union(date.toDateString(), msg);
+
+                socket.broadcast.emit('chat message', broadcastMessage); // отправить всем кроме меня
+                socket.emit('my chat message', message); // отправить только мне
+            });
+
+            socket.on('disconnect', function () {
+                let clientID = socket.client.id;
+                storeUsers.del(clientID);
+                socket.broadcast.emit('update users', storeUsers.json(null, 20));
+                // console.log('Got disconnect! id: '+ clientID.toString());
+                socket.disconnect(true);
+            });
+        });
     }
 }
