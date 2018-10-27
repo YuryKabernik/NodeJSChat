ecf272becd08f24c1e4152bb4135a571345e9a32
AliaksanderOdinec <06sasha96@mail.ru>
2018-10-27 20:41:44 +0300

    Add loading login form on page

---
+++ NodeJSChat/public/javascript/chat-member-logic.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+$(document).ready(function () {
+
+    // Get the modal login form
+    $.get('/login', function (response, status) {
+        $('.loginWrapper').append(response);
+    });
+});
