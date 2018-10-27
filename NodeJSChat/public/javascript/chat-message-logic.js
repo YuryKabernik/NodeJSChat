ff886f94d0e22d5e19190ce7db51fcf1e4edd357
Yury Kabernik-Berazouski <kobernykbeljr@gmail.com>
2018-10-27 21:04:13 +0300

    Add sockets events processing in chat-message-logic.js

--- NodeJSChat/public/javascript/chat-message-logic.js
+++ NodeJSChat/public/javascript/chat-message-logic.js
@@ -1,4 +1,6 @@
 $(function () {
+    var socket = io();
+    
     $('form').submit(function () {
         var message = $('#m').val();
         var userName = $('#name').text();
@@ -14,5 +16,17 @@ $(function () {
             document.getElementById('id01').style.display = 'block';
         }
         return false;
+    });
+    
+    socket.on('my chat message', function (msg) {
+        var msgs_container = $('#messages');
+        msgs_container.append($('<li>').html(msg));
+        msgs_container.scrollTop(msgs_container.scrollTop() + 300);
+    });
+
+    socket.on('chat message', function (msg) {
+        var msgs_container = $('#messages');
+        msgs_container.append($('<li>').html(msg));
+        msgs_container.scrollTop(msgs_container.scrollTop() + 300);
     });
 });
