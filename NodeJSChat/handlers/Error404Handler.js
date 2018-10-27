b4959353b4c0af599bc1b227cefce3803a9e3542
Yury Kabernik-Berazouski <kobernykbeljr@gmail.com>
2018-10-27 14:15:39 +0300

    Add 404 error handler

---
+++ NodeJSChat/handlers/Error404Handler.js
@@ -0,0 +1,8 @@
+﻿import ErrorHandler from "./base/ErrorHandler";
+
+export default class Error404Handler extends ErrorHandler{
+
+    constructor() {
+        super(401, 'Page not found.');
+    }
+}
