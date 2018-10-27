582135515042142f99d2f3f8347f83847c757fe1
Yury Kabernik-Berazouski <kobernykbeljr@gmail.com>
2018-10-27 14:17:52 +0300

    Add  500 error handler

---
+++ NodeJSChat/handlers/Error500Handler.js
@@ -0,0 +1,8 @@
+﻿import ErrorHandler from './base/ErrorHandler';
+
+export default class Error500Handler extends ErrorHandler {
+
+    constructor() {
+        super(500, 'Internal server error.');
+    }
+}
