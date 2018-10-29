8bbfe760cb631145308c047893499e35d4c5e7e3
Yury Kabernik-Berazouski <kobernykbeljr@gmail.com>
2018-10-29 21:36:29 +0300

    Add routing to users test

--- NodeJSChat/tests/Routes.test.js
+++ NodeJSChat/tests/Routes.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const Store = require('data-store');
 var app = require('../starter.js').app;
 
-describe('MessageRoute class testing', function () {
+describe('Routing testing', function () {
 
     it('should return list ot the messages with as JSON string', function (done) {
         // Arrange
@@ -12,6 +12,18 @@ describe('MessageRoute class testing', function () {
         // Act
         let res = request(app)
             .get('/messages')
+            .expect(200, expectedResult) // Assert
+            .end(done);
+    });
+
+    it('should return list ot the users with as JSON string', function (done) {
+        // Arrange
+        let store = new Store({ path: './data_storage/users.json' });
+        let expectedResult = store.json(null, 10);
+
+        // Act
+        let res = request(app)
+            .get('/users')
             .expect(200, expectedResult) // Assert
             .end(done);
     });
